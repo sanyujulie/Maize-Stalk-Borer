12631e1fdabc05add9cd0d22c7dec848fa2903f8
sanyujulie <sanyjulie0@gmail.com>
2024-06-07 17:32:29 +0300

    deploying

--- MSBDS/ssms_project/settings.py
+++ MSBDS/ssms_project/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -138,11 +139,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = BASE_DIR/'assets'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'Services/static'),
 ]
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # configurations for file storage
 # https://docs.djangoproject.com/en/4.2/topics/files/
 #  
