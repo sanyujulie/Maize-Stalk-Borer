bf8825bac033c0ae177c4961256385430287d7d7
sanyujulie <sanyjulie0@gmail.com>
2024-06-10 16:18:12 +0300

    settings changed

--- MSBDS/ssms_project/settings.py
+++ MSBDS/ssms_project/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
-import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -87,21 +87,14 @@ WSGI_APPLICATION = 'ssms_project.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 
-    'default': dj_database_url.config(
-       default='postgres://database_postgre_2bu6_user:QUjL4v2rESqsJgp1DJRS8g3eEF5NLLMW@dpg-cpjelnkf7o1s73bv0at0-a.oregon-postgres.render.com/database_postgre_2bu6',
-        conn_max_age=600 
-    )
+   
 }
-if RENDER_EXTERNAL_HOSTNAME:
-    DATABASES['default'] = dj_database_url.config(
-        default='postgres://database_postgre_2bu6_user:QUjL4v2rESqsJgp1DJRS8g3eEF5NLLMW@dpg-cpjelnkf7o1s73bv0at0-a/database_postgre_2bu6',
-        conn_max_age=600
-    )
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
