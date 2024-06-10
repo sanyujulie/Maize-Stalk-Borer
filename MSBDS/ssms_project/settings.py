d808a1da4f482154c155d0a2ff1d57c91fd13f06
sanyujulie <sanyjulie0@gmail.com>
2024-06-10 13:57:36 +0300

    update

--- MSBDS/ssms_project/settings.py
+++ MSBDS/ssms_project/settings.py
@@ -135,10 +135,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR/'assets'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR/'static'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, '/static'),
+    os.path.join(BASE_DIR, 'Services/static'),
 ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
