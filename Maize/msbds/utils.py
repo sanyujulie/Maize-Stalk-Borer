e71d7e0f2877c224552dec7ea6426e6ff0d36a79
sanyujulie <sanyjulie0@gmail.com>
2024-06-12 13:39:31 +0300

    changes to the file

--- Maize/msbds/utils.py
+++ Maize/msbds/utils.py
@@ -1,13 +1,16 @@
 from geopy.geocoders import Nominatim
-from geopy.exc import GeocoderTimedOut
+from geopy.exc import GeocoderTimedOut, GeocoderUnavailable
+import time
 
-def get_lat_long(location):
-    geolocator = Nominatim(user_agent="Services")
-    try:
-        location = geolocator.geocode(location)
-        if location:
-            return location.latitude, location.longitude
-        else:
-            return None, None
-    except GeocoderTimedOut:
-        return None, None
+geolocator = Nominatim(user_agent="msbds")
+
+def get_lat_long(location, retries=3, timeout=30):
+    for attempt in range(retries):
+        try:
+            location = geolocator.geocode(location, timeout=timeout)
+            if location:
+                return location.latitude, location.longitude
+        except (GeocoderTimedOut, GeocoderUnavailable) as e:
+            print(f"Attempt {attempt+1} failed with error: {e}")
+            time.sleep(2)  # Wait a bit before retrying
+    raise GeocoderUnavailable(f"Geocoding service is unavailable after {retries} retries.")
